bcace96ec40c9a1a9269ec2473fde7871c2d81c5
francois.bruere <francois.bruere@gmail.com>
2021-11-21 19:17:17 +0200

    Add AccountController - Add Common for SD Static Detail - Add RegistrationResponseDTO - Add AuthenticationDTO - Add AuthenticationResponseDTO Implement Methods for signup signin getcredentials get claims. - HttpPost - AllowAnonymous

---
+++ CrimeLogger/Shared/AuthenticationResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeLogger.Shared
+{
+    public class AuthenticationResponseDTO
+    {
+        public bool isAuthSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public string Token { get; set; }
+        public UserDTO UserDTO { get; set; }
+
+    }
+}
---
+++ CrimeLogger/Shared/RegistrationResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrimeLogger.Shared
+{
+    public class RegistrationResponseDTO
+    {
+
+        public bool isRegistrationSuccessful { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+
+
+    }
+}
